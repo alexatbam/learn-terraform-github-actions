89bd81f646d59d74f6a397b871f4bb337721768c
alexatbam <alex@barkingadmedia.com>
2021-12-08 15:14:32 +0200

    t1234567

--- main.tf
+++ main.tf
@@ -5,26 +5,13 @@ terraform {
   }
 }
 
-locals {
-  environments_project = {
-    dev    = "bam-big-data-dev",
-    prodga = "bam-big-data-312108"
-  }
-
-  env_id = terraform.workspace
-
-  project_id = lookup(local.environments_project, terraform.workspace)
-  region     = "us-central1"
-  zone       = "us-central1-c"
-  bq_loction = "US"
-}
-
 provider "google" {
-  project = local.project_id
-  region  = local.region
-  zone    = local.zone
+  project = "bam-big-data-dev"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
 
 resource "google_storage_bucket" "test_gh" {
   name          = "test_gh_demo"
+  location = "US"
 }
